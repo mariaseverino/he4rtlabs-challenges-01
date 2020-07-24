e1e77094093ad81cdaf80c7cb59ee173f5abf238
Maria Severino <mariarsseverino@gmail.com>
2020-07-24 01:31:56 -0300

    arrumando o calculo e fazendo melhorias

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var valorProjeto = document.querySelector('#valor_projeto')
 var horasTrabalhadas = document.querySelector('#horas_trabalhdas')
 var diasEfetivos = document.querySelector('#dias_efetivos')
 var ferias = document.querySelector('#ferias')
-var valor = document.querySelector('h1')
+var valor = document.querySelector('.botao_e_resultado h1')
 
 function calcular(){
     
@@ -11,6 +11,9 @@ function calcular(){
 
     if (!isNaN(valorHora)){
         valor.innerHTML = `R$ ${valorHora.toFixed(2)}`
+    }
+    else{
+        valor.innerHTML = 'R$ 0,00'
     }
     
     valorProjeto.value = ''
