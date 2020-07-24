aa6715bdf92b47e4cbc41fd972c1ca51f4ccf34a
Maria Severino <mariarsseverino@gmail.com>
2020-07-23 23:46:44 -0300

    adicionado calculo do valor hora

---
+++ src/main.js
@@ -0,0 +1,20 @@
+var botao = document.querySelector('button')
+var valorProjeto = document.querySelector('#valor_projeto')
+var horasTrabalhadas = document.querySelector('#horas_trabalhdas')
+var diasEfetivos = document.querySelector('#dias_efetivos')
+var ferias = document.querySelector('#ferias')
+var valor = document.querySelector('h1')
+
+function calcular(){
+    
+    var valorHora = (valorProjeto.value / (diasEfetivos.value * 4 * horasTrabalhadas.value)) + (ferias.value * diasEfetivos.value * horasTrabalhadas.value)
+
+    if (!isNaN(valorHora)){
+        valor.innerHTML = `R$ ${valorHora.toFixed(2)}`
+    }
+    
+    valorProjeto.value = ''
+    diasEfetivos.value = ''
+    horasTrabalhadas.value = ''
+    ferias.value = ''
+}
